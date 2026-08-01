29f1179614bfd5a43d7c360b2faaa15821bad112
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2026-08-01 16:00:12 +0000

    Data Types

---
+++ 01_basics/02_Datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // treat as all new js Code as newer Version
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log("Yogesh");
+console.log(3 
+    +
+     3); // code readily should be maintianed
+
+let name = "yogesh"
+let age = 23
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+//string => ".."
+// boolen => true/false
+// null   => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+
+
+// object   null is an object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)  // object
