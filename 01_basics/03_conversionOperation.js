c300ae20b9b756f3f1afe7344c537cbd1732b6d8
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2026-08-01 16:50:29 +0000

    post fix and prefix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,7 +17,7 @@ let score = "yogesh"
 let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1=> true  ; 0 => false;
 // " " => false
@@ -26,4 +26,42 @@ console.log(booleanIsLoggedIn)
 let someNumber =33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
+
+
+//****************    Operations   *******************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue)  => -3
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str = " yogesh"
+
+let str3 = str1 + str
+// console.log(str3)
+
+// console.log("1"+2)//12
+// console.log(1 +"2") //12
+// console.log("1" +2 +2) //"1" -> makes eveeryoe string 122
+// console.log(1 +2 +"2")// 1 is number so 1+2 = 32
+// console.log(1 +"3" +2)//132
+
+
+// console.log(+true)
+// console.log(+"")
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter)
+
+//Link to study about postfix and prefix : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
