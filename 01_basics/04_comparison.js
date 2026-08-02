1016406a1736e33ae483697e0efd2e31c3b785fa
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2026-08-02 14:42:59 +0000

    4_comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+console.log(2>1)
+console.log(2>=1)
+
+console.log("2">1) //  "2" is converted to 2
+console.log("02">1) 
+
+
+console.log(null>0);
+console.log(null == 0);
+console.log(null>=0);
+// comparison operator > < >= <= conversts null to 0 so 
+//false
+//false
+//true
+
+console.log(undefined == 0);
+console.log(undefined >0);
+console.log(undefined <0);
+
+// ===
+
+console.log("2"===2);
+
+
+
+
+
+
