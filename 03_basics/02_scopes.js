7ccf66a65139110cc7c993ffacf98107fe1dfe72
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2026-08-11 16:40:59 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,21 @@
+// var c = 300
+
+//{ scope }
+let a =300
+if (true) {
+    let a = 10
+    const b = 20
+    console.log("INNER",a); // Block Scope
+     
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+
+
+console.log(a);
+// console.log(b);
+
