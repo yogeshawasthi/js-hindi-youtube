7d5b734780feacbf23b2fa041432e0a09c83000b
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2026-08-19 05:50:10 +0000

    iffe to run automatizally

---
+++ 03_basics/04_iffe.js
@@ -0,0 +1,16 @@
+// IMMEDIATELY INVOKED FUNCTION EXPRESSION (IIFE)
+
+
+(function chai(){
+    console.log(`DB CONNECTED`);
+    
+
+})();
+ // use semicolon to make it look after down
+
+//(funcation defeiniton) (function call)
+
+(  (name)=> {
+    console.log(`DB CONNECTED SUCESSFULLY TWO ${name}`);
+    
+}) ('yogesh')
