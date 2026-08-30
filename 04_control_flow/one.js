a23f0eef4cd2c96322233ce132a3f03cd33feb2d
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2026-08-30 17:26:51 +0000

    Contorl flow upto 25 min new file is being created

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,7 +1,66 @@
-//if
+// //if
 
-if(true){
+// const isUserLoggedIn = true
+// const temperature = 41
+
+// if(temperature === 40){
+//     console.log("equal than 41")
+// }else{
+//     console.log("temperature is greater than 50 ");
+// }
+
+// console.log("Execute surely ");
+
+
+// 2<=2
+
+
+
+// 3!=2
+
+// <, >, <= , != , ==, === , !==
+
+
+// const score= 200
+
+// if(score>100){
+//     let power = "fly"
+//     console.log(`User Power : ${power}`);
     
-    // goes inside only when true but not for true
+// }
+// console.log(`User Power : ${power}`);
 
+
+// const balance =1000
+
+// // if (balance>500)  console.log("test") ,console.log("tesedt officealyy started");
+// //   two logs not be wirtten together single can be used 
+
+// if (balance <500) {
+//     console.log("Less than 500");
+    
+    
+// } else if(balance<750) {
+//      console.log("Less than 750");
+// } else if (balance<900) {
+//      console.log("Less than 900");
+// }else{
+//     console.log("less than 1200");
+    
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFroGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 3===3) {
+    console.log("Allow to buy course");
+       
+}
+
+if (loggedInFroGoogle || loggedInFromEmail ) {
+    console.log("user logged in ");
+    
 }
