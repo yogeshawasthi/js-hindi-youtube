b425f3f181d09963c553901094b29e2cd5d84811
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2026-09-01 18:10:36 +0000

    while and do while loop done

---
+++ 05_Itterations/two.js
@@ -0,0 +1,26 @@
+let index = 0
+while (index<=10) {
+    // console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+
+let myArray = ['batman', 'flash', 'superman']
+
+let arr = 0
+
+while (arr< myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+
+// do while loop 
+
+let score = 10
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<=10);
